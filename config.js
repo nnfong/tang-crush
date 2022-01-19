20ce8fa587dd3bc701c443dfff72cdeb4d5c9b49
nnfong <98043963+nnfong@users.noreply.github.com>
2022-01-20 00:46:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,8 @@ const CONFIG = {
     desc: 'Rất cute đúng khom ',
     btnYes: 'chủn <33',
     btnNo: 'Không cute tí nào',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    question:'Trên thế giới hơn 7 tỉ người mà phong lại đyeu z ',
+    btnReply: 'phong rấc cute',
+    reply: 'tanh khom cute tí nào <33333333',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
