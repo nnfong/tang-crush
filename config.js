14bd47774916a553c3562c2ce3e7f4df0e6aa0c3
nnfong <98043963+nnfong@users.noreply.github.com>
2022-02-09 21:52:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,2 @@
-const CONFIG = {
-    introTitle: 'tanh béo!',
-    introDesc: 'Tại sao phong lại đyeu z`',
-    btnIntro: 'hihi',
-    title: 'Phong rấc cute 😙',
-    desc: 'Rất cute đúng khom ',
-    btnYes: 'chủn <33',
-    btnNo: 'Không cute tí nào',
-    question:'Trên thế giới hơn 7 tỉ người mà phong lại đyeu z ',
-    btnReply: 'phong rấc cute',
-    reply: 'tanh khom cute tí nào <33333333',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+
 }
